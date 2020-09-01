d54fcd26211a7463afd10c883cc4d3485f3647d2
fauky <fmented@gmail.com>
2020-09-02 05:06:31 +0700

    added minified script, added incomplete chords

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,30 @@
 var chord;
+
 var t = document.getElementById("txt");
+
 var alt = document.getElementById("alt");
+
 var nav = document.getElementById('navbar')
+
 var lo, hi;
+
 var midisupport;
+
 var tm;
+
 var played = [];
+
 var notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
 var p;
+
 var inputDev = [];
+
 const synth = new Tone.PolySynth({
   polyphony:2,
   voice: Tone.Synth,
 }).toDestination();
+
 const synth2 = new Tone.PolySynth({
   polyphony:2,
   voice: Tone.Synth,
@@ -26,11 +38,11 @@ else{
   Tone.start()
 }
 }
+
 var chords = [
   { name: "M", keys: [0, 4, 7] },
   { name: "-5", keys: [0, 4, 6] },
-  { name: "+5", keys: [0, 4, 8] },
-  
+  { name: "Aug", keys: [0, 4, 8] },
   
   { name: "(add2)", keys: [0, 2, 4, 7] },
   { name: "(add4)", keys: [0, 4, 5, 7] },
@@ -40,7 +52,6 @@ var chords = [
   { name: "(add11)", keys: [0, 4, 7, 17] },
   { name: "(add13)", keys: [0, 4, 7, 21] },
   
-  
   { name: "-5(add2)", keys: [0, 2, 4, 6] },
   { name: "-5(add4)", keys: [0, 4, 5, 6] },
   { name: "-5(add+5)", keys: [0, 4, 6, 8] },
@@ -48,14 +59,12 @@ var chords = [
   { name: "-5(add11)", keys: [0, 4, 6, 17] },
   { name: "-5(add13)", keys: [0, 4, 6, 21] },
   
-  
-  { name: "+5(add2)", keys: [0, 2, 4, 8] },
-  { name: "+5(add4)", keys: [0, 4, 5, 8] },
-  { name: "+5(add-5)", keys: [0, 4, 6, 8] },
-  { name: "+5(add9)", keys: [0, 4, 8, 14] },
-  { name: "+5(add11)", keys: [0, 4, 8, 17] },
-  { name: "+5(add13)", keys: [0, 4, 8, 21] },
-  
+  { name: "Aug(add2)", keys: [0, 2, 4, 8] },
+  { name: "Aug(add4)", keys: [0, 4, 5, 8] },
+  { name: "Aug(add-5)", keys: [0, 4, 6, 8] },
+  { name: "Aug(add9)", keys: [0, 4, 8, 14] },
+  { name: "Aug(add11)", keys: [0, 4, 8, 17] },
+  { name: "Aug(add13)", keys: [0, 4, 8, 21] },
   
   { name: "6", keys: [0, 4, 7, 9] },
   { name: "M7", keys: [0, 4, 7, 11] },
@@ -63,19 +72,17 @@ var chords = [
   { name: "M11", keys: [0, 4, 7, 11, 14, 17] },
   { name: "M13", keys: [0, 4, 7, 11, 14, 17, 21] },
   
-  
   { name: "M6-5", keys: [0, 4, 6, 9] },
   { name: "M7-5", keys: [0, 4, 6, 11] },
   { name: "M9-5", keys: [0, 4, 6, 11, 14] },
   { name: "M11-5", keys: [0, 4, 6, 11, 14, 17] },
   { name: "M13-5", keys: [0, 4, 6, 11, 14, 17, 21] },
   
-  
   { name: "M6+5", keys: [0, 4, 8, 9] },
   { name: "M7+5", keys: [0, 4, 8, 11] },
-  { name: "M9+5", keys: [0, 4, 8, 14] },
-  { name: "M11+5", keys: [0, 4, 8, 14, 17] },
-  { name: "M13+5", keys: [0, 4, 8, 14, 17, 21] },
+  { name: "M9+5", keys: [0, 4, 8, 11, 14] },
+  { name: "M11+5", keys: [0, 4, 8, 11, 14, 17] },
+  { name: "M13+5", keys: [0, 4, 8, 11, 14, 17, 21] },
   
   { name: "7", keys: [0, 4, 7, 10] },
   { name: "9", keys: [0, 4, 7, 10, 14] },
@@ -93,10 +100,8 @@ var chords = [
   { name: "13+5", keys: [0, 4, 8, 10, 14, 17, 21] },
   
   
-  
   { name: "m", keys: [0, 3, 7] },
   { name: "m+5", keys: [0, 3, 8] },
-  
   
   { name: "m(add2)", keys: [0, 2, 3, 7] },
   { name: "m(add4)", keys: [0, 3, 5, 7] },
@@ -106,14 +111,12 @@ var chords = [
   { name: "m(add11)", keys: [0, 3, 7, 17] },
   { name: "m(add13)", keys: [0, 3, 7, 21] },
   
-  
   { name: "m-5(add2)", keys: [0, 2, 3, 6] },
   { name: "m-5(add4)", keys: [0, 3, 5, 6] },
   { name: "m-5(add+5)", keys: [0, 3, 6, 8] },
   { name: "m-5(add9)", keys: [0, 3, 6, 14] },
   { name: "m-5(add11)", keys: [0, 3, 6, 17] },
   { name: "m-5(add13)", keys: [0, 3, 6, 21] },
-  
   
   { name: "m+5(add2)", keys: [0, 2, 3, 8] },
   { name: "m+5(add4)", keys: [0, 3, 5, 8] },
@@ -122,13 +125,11 @@ var chords = [
   { name: "m+5(add11)", keys: [0, 3, 8, 17] },
   { name: "m+5(add13)", keys: [0, 3, 8, 21] },
   
-  
   { name: "m6", keys: [0, 3, 7, 9] },
   { name: "mM7", keys: [0, 3, 7, 11] },
   { name: "mM9", keys: [0, 3, 7, 11, 14] },
   { name: "mM11", keys: [0, 3, 7, 11, 14, 17] },
   { name: "mM13", keys: [0, 3, 7, 11, 14, 17, 21] },
-  
   
   { name: "mM6-5", keys: [0, 3, 6, 9] },
   { name: "mM7-5", keys: [0, 3, 6, 11] },
@@ -136,13 +137,11 @@ var chords = [
   { name: "mM11-5", keys: [0, 3, 6, 11, 14, 17] },
   { name: "mM13-5", keys: [0, 3, 6, 11, 14, 17, 21] },
   
-  
   { name: "mM6+5", keys: [0, 3, 8, 9] },
   { name: "mM7+5", keys: [0, 3, 8, 11] },
-  { name: "mM9+5", keys: [0, 3, 8, 14] },
-  { name: "mM11+5", keys: [0, 3, 8, 14, 17] },
-  { name: "mM13+5", keys: [0, 3, 8, 14, 17, 21] },
-  
+  { name: "mM9+5", keys: [0, 3, 8, 11, 14] },
+  { name: "mM11+5", keys: [0, 3, 8, 11, 14, 17] },
+  { name: "mM13+5", keys: [0, 3, 8, 11, 14, 17, 21] },
   
   { name: "m7", keys: [0, 3, 7, 10] },
   { name: "m9", keys: [0, 3, 7, 10, 14] },
@@ -222,9 +221,42 @@ var chords = [
   { name: "m6/11(no5)", keys: [0, 3, 9, 17] },
   { name: "m6/13(no5)", keys: [0, 3, 9, 21] },
   
+  
+  { name: "M9(no7)", keys: [0, 4, 7, 14] },
+  { name: "M11(no7)", keys: [0, 4, 7, 14, 17] },
+  { name: "M13(no7)", keys: [0, 4, 7, 14, 17, 21] },
+  
+  { name: "M9-5(no7)", keys: [0, 4, 6, 14] },
+  { name: "M11-5(no7)", keys: [0, 4, 6, 14, 17] },
+  { name: "M13-5(no7)", keys: [0, 4, 6, 14, 17, 21] },
+  
+  { name: "M9+5(no7)", keys: [0, 4, 8, 14] },
+  { name: "M11+5(no7)", keys: [0, 4, 8, 14, 17] },
+  { name: "M13+5(no7)", keys: [0, 4, 8, 14, 17, 21] },
+  
+  { name: "mM9(no7)", keys: [0, 3, 7, 14] },
+  { name: "mM11(no7)", keys: [0, 3, 7, 14, 17] },
+  { name: "mM13(no7)", keys: [0, 3, 7, 14, 17, 21] },
+  
+  { name: "mM9-5(no7)", keys: [0, 3, 6, 14] },
+  { name: "mM11-5(no7)", keys: [0, 3, 6, 14, 17] },
+  { name: "mM13-5(no7)", keys: [0, 3, 6, 14, 17, 21] },
+  
+  { name: "mM9+5(no7)", keys: [0, 3, 8, 14] },
+  { name: "mM11+5(no7)", keys: [0, 3, 8, 14, 17] },
+  { name: "mM13+5(no7)", keys: [0, 3, 8, 14, 17, 21] },
+  
+  { name: "M9(no5)(no7)", keys: [0, 4, 14] },
+  { name: "M11(no5)(no7)", keys: [0, 4, 14, 17] },
+  { name: "M13(no5)(no7)", keys: [0, 4, 14, 17, 21] },
+  
+  { name: "mM9(no5)(no7)", keys: [0, 3, 14] },
+  { name: "mM11(no5)(no7)", keys: [0, 3, 14, 17] },
+  { name: "mM13(no5)(no7)", keys: [0, 3, 14, 17, 21] },
+  
+  
   ];
   
-
 function setLow(n) {
   lo = n;
   t.innerHTML = "Press the highest note";
@@ -251,6 +283,7 @@ navigator.permissions.query({ name: "midi" }).then((PermissionStatus) => {
 function onFailed() {
   t.innerHTML = "This Browser Doesn't Support MIDI";
 }
+
 function compare(a, b) {
   if (a.toString() == b.toString()) {
     return true;
@@ -292,7 +325,6 @@ else{
 
 }
 }
-
 
 function getChordList() {
   var name, keys, nm;
@@ -365,8 +397,6 @@ function getChordList() {
   t.innerHTML = "Ready";
   return list;
 }
-
-
 
 function getMIDIMessage(midiMessage) {
   var e = midiMessage.data[1];
--- chord.list.js
+++ chord.list.js
@@ -1,9 +1,10 @@
+//Just a Helper File
+
 obj=
 [
 { name: "M", keys: [0, 4, 7] },
 { name: "-5", keys: [0, 4, 6] },
-{ name: "+5", keys: [0, 4, 8] },
-
+{ name: "Aug", keys: [0, 4, 8] },
 
 { name: "(add2)", keys: [0, 2, 4, 7] },
 { name: "(add4)", keys: [0, 4, 5, 7] },
@@ -13,7 +14,6 @@ obj=
 { name: "(add11)", keys: [0, 4, 7, 17] },
 { name: "(add13)", keys: [0, 4, 7, 21] },
 
-
 { name: "-5(add2)", keys: [0, 2, 4, 6] },
 { name: "-5(add4)", keys: [0, 4, 5, 6] },
 { name: "-5(add+5)", keys: [0, 4, 6, 8] },
@@ -21,14 +21,12 @@ obj=
 { name: "-5(add11)", keys: [0, 4, 6, 17] },
 { name: "-5(add13)", keys: [0, 4, 6, 21] },
 
-
-{ name: "+5(add2)", keys: [0, 2, 4, 8] },
-{ name: "+5(add4)", keys: [0, 4, 5, 8] },
-{ name: "+5(add-5)", keys: [0, 4, 6, 8] },
-{ name: "+5(add9)", keys: [0, 4, 8, 14] },
-{ name: "+5(add11)", keys: [0, 4, 8, 17] },
-{ name: "+5(add13)", keys: [0, 4, 8, 21] },
-
+{ name: "Aug(add2)", keys: [0, 2, 4, 8] },
+{ name: "Aug(add4)", keys: [0, 4, 5, 8] },
+{ name: "Aug(add-5)", keys: [0, 4, 6, 8] },
+{ name: "Aug(add9)", keys: [0, 4, 8, 14] },
+{ name: "Aug(add11)", keys: [0, 4, 8, 17] },
+{ name: "Aug(add13)", keys: [0, 4, 8, 21] },
 
 { name: "6", keys: [0, 4, 7, 9] },
 { name: "M7", keys: [0, 4, 7, 11] },
@@ -36,19 +34,17 @@ obj=
 { name: "M11", keys: [0, 4, 7, 11, 14, 17] },
 { name: "M13", keys: [0, 4, 7, 11, 14, 17, 21] },
 
-
 { name: "M6-5", keys: [0, 4, 6, 9] },
 { name: "M7-5", keys: [0, 4, 6, 11] },
 { name: "M9-5", keys: [0, 4, 6, 11, 14] },
 { name: "M11-5", keys: [0, 4, 6, 11, 14, 17] },
 { name: "M13-5", keys: [0, 4, 6, 11, 14, 17, 21] },
 
-
 { name: "M6+5", keys: [0, 4, 8, 9] },
 { name: "M7+5", keys: [0, 4, 8, 11] },
-{ name: "M9+5", keys: [0, 4, 8, 14] },
-{ name: "M11+5", keys: [0, 4, 8, 14, 17] },
-{ name: "M13+5", keys: [0, 4, 8, 14, 17, 21] },
+{ name: "M9+5", keys: [0, 4, 8, 11, 14] },
+{ name: "M11+5", keys: [0, 4, 8, 11, 14, 17] },
+{ name: "M13+5", keys: [0, 4, 8, 11, 14, 17, 21] },
 
 { name: "7", keys: [0, 4, 7, 10] },
 { name: "9", keys: [0, 4, 7, 10, 14] },
@@ -66,10 +62,8 @@ obj=
 { name: "13+5", keys: [0, 4, 8, 10, 14, 17, 21] },
 
 
-
 { name: "m", keys: [0, 3, 7] },
 { name: "m+5", keys: [0, 3, 8] },
-
 
 { name: "m(add2)", keys: [0, 2, 3, 7] },
 { name: "m(add4)", keys: [0, 3, 5, 7] },
@@ -79,14 +73,12 @@ obj=
 { name: "m(add11)", keys: [0, 3, 7, 17] },
 { name: "m(add13)", keys: [0, 3, 7, 21] },
 
-
 { name: "m-5(add2)", keys: [0, 2, 3, 6] },
 { name: "m-5(add4)", keys: [0, 3, 5, 6] },
 { name: "m-5(add+5)", keys: [0, 3, 6, 8] },
 { name: "m-5(add9)", keys: [0, 3, 6, 14] },
 { name: "m-5(add11)", keys: [0, 3, 6, 17] },
 { name: "m-5(add13)", keys: [0, 3, 6, 21] },
-
 
 { name: "m+5(add2)", keys: [0, 2, 3, 8] },
 { name: "m+5(add4)", keys: [0, 3, 5, 8] },
@@ -95,13 +87,11 @@ obj=
 { name: "m+5(add11)", keys: [0, 3, 8, 17] },
 { name: "m+5(add13)", keys: [0, 3, 8, 21] },
 
-
 { name: "m6", keys: [0, 3, 7, 9] },
 { name: "mM7", keys: [0, 3, 7, 11] },
 { name: "mM9", keys: [0, 3, 7, 11, 14] },
 { name: "mM11", keys: [0, 3, 7, 11, 14, 17] },
 { name: "mM13", keys: [0, 3, 7, 11, 14, 17, 21] },
-
 
 { name: "mM6-5", keys: [0, 3, 6, 9] },
 { name: "mM7-5", keys: [0, 3, 6, 11] },
@@ -109,13 +99,11 @@ obj=
 { name: "mM11-5", keys: [0, 3, 6, 11, 14, 17] },
 { name: "mM13-5", keys: [0, 3, 6, 11, 14, 17, 21] },
 
-
 { name: "mM6+5", keys: [0, 3, 8, 9] },
 { name: "mM7+5", keys: [0, 3, 8, 11] },
-{ name: "mM9+5", keys: [0, 3, 8, 14] },
-{ name: "mM11+5", keys: [0, 3, 8, 14, 17] },
-{ name: "mM13+5", keys: [0, 3, 8, 14, 17, 21] },
-
+{ name: "mM9+5", keys: [0, 3, 8, 11, 14] },
+{ name: "mM11+5", keys: [0, 3, 8, 11, 14, 17] },
+{ name: "mM13+5", keys: [0, 3, 8, 11, 14, 17, 21] },
 
 { name: "m7", keys: [0, 3, 7, 10] },
 { name: "m9", keys: [0, 3, 7, 10, 14] },
@@ -194,5 +182,39 @@ obj=
 { name: "m6/9(no5)", keys: [0, 3, 9, 14] },
 { name: "m6/11(no5)", keys: [0, 3, 9, 17] },
 { name: "m6/13(no5)", keys: [0, 3, 9, 21] },
+
+
+{ name: "M9(no7)", keys: [0, 4, 7, 14] },
+{ name: "M11(no7)", keys: [0, 4, 7, 14, 17] },
+{ name: "M13(no7)", keys: [0, 4, 7, 14, 17, 21] },
+
+{ name: "M9-5(no7)", keys: [0, 4, 6, 14] },
+{ name: "M11-5(no7)", keys: [0, 4, 6, 14, 17] },
+{ name: "M13-5(no7)", keys: [0, 4, 6, 14, 17, 21] },
+
+{ name: "M9+5(no7)", keys: [0, 4, 8, 14] },
+{ name: "M11+5(no7)", keys: [0, 4, 8, 14, 17] },
+{ name: "M13+5(no7)", keys: [0, 4, 8, 14, 17, 21] },
+
+{ name: "mM9(no7)", keys: [0, 3, 7, 14] },
+{ name: "mM11(no7)", keys: [0, 3, 7, 14, 17] },
+{ name: "mM13(no7)", keys: [0, 3, 7, 14, 17, 21] },
+
+{ name: "mM9-5(no7)", keys: [0, 3, 6, 14] },
+{ name: "mM11-5(no7)", keys: [0, 3, 6, 14, 17] },
+{ name: "mM13-5(no7)", keys: [0, 3, 6, 14, 17, 21] },
+
+{ name: "mM9+5(no7)", keys: [0, 3, 8, 14] },
+{ name: "mM11+5(no7)", keys: [0, 3, 8, 14, 17] },
+{ name: "mM13+5(no7)", keys: [0, 3, 8, 14, 17, 21] },
+
+{ name: "M9(no5)(no7)", keys: [0, 4, 14] },
+{ name: "M11(no5)(no7)", keys: [0, 4, 14, 17] },
+{ name: "M13(no5)(no7)", keys: [0, 4, 14, 17, 21] },
+
+{ name: "mM9(no5)(no7)", keys: [0, 3, 14] },
+{ name: "mM11(no5)(no7)", keys: [0, 3, 14, 17] },
+{ name: "mM13(no5)(no7)", keys: [0, 3, 14, 17, 21] },
+
 
 ]
